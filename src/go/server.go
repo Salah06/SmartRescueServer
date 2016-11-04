9e687e247874b2d4d6c6a2e1304513e3abc1094f
Simon PARIS <ps205947@etu.unice.fr>
2016-11-04 10:57:56 +0100

    heandle client java

--- src/go/server.go
+++ src/go/server.go
@@ -9,13 +9,44 @@
 package main
 
 import (
-    "net"
-    "bufio"
     "log"
     "fmt"
+    "gopkg.in/zabawaba99/firego.v1"
 )
 
-// +slide
+
+var f = firego.New("https://testrescue-d8b04.firebaseio.com/", nil)
+
+func pushValue() {
+    v := "bar"
+    pushedFirego, err := f.Push(v)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var bar string
+    if err := pushedFirego.Value(&bar); err != nil {
+        log.Fatal(err)
+    }
+
+    // prints "https://my-firebase-app.firebaseIO.com/-JgvLHXszP4xS0AUN-nI: bar"
+    fmt.Printf("%s: %s\n", pushedFirego, bar)
+}
+
+func getValue() {
+    var v map[string]interface{}
+    if err := f.Value(&v); err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%s\n", v)
+}
+
+func CheckError(err error) {
+    if err  != nil {
+        fmt.Println("Error: " , err)
+    }
+}
+
 func handleConnection(conn net.Conn) {
     defer conn.Close()
 
@@ -31,6 +62,10 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
+    //f.Auth("null")
+    //pushValue()
+    //getValue()
+
     listener, err := net.Listen("tcp", "localhost:1234")
     if err != nil {
         log.Fatal(err)
@@ -45,11 +80,3 @@ func main() {
         go handleConnection(conn)
     }
 }
-// ---
-// Local Variables:
-// compile-command: "go run *.go"
-// End:
-
-// pour tester : nc localhost 1234
-
-// on met go pour lancer une fonction en arriere plan (goroutine)
