818a9e555889db8bc082e6ffc276e02a0f20c88e
Simon PARIS <ps205947@etu.unice.fr>
2016-11-16 16:38:32 +0100

    po

--- main.go
+++ main.go
@@ -14,14 +14,21 @@ func handleAndroidClient(w http.ResponseWriter, r *http.Request) {
     vehiculeId := r.FormValue("vehiculeId")
     token := r.FormValue("token")
     fmt.Printf(vehiculeId)
-    fmt.Printf("coucou")
     fmt.Printf(token)
+}
+
+func handleJavaClient(w http.ResponseWriter, r *http.Request) {
+    lvl := r.FormValue("emergencyLevel")
+    address := r.FormValue("address")
+    service := r.FormValue("service")
+    fmt.Println("%s, %s, %s", service, lvl, address)
 }
 
 func main() {
 
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/android", handleAndroidClient).Methods("POST")
+    router.HandleFunc("/java", handleJavaClient).Methods("POST")
 
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
