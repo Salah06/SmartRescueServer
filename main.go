dee14a820171cf11d4c09b029d7fa0963cbd81fd
Simon PARIS <ps205947@etu.unice.fr>
2017-01-12 21:09:13 +0100

    main.go creat emergency room. READY FOR integration and tests

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ const (
 var firebase = firego.New("https://smartrescue-6e8ce.firebaseio.com/", nil)
 var memo map[string][]string
 var tokens []string
+var repartiteur map[string]chan([]string)
 
 
 func handleJavaClient(w http.ResponseWriter, r *http.Request) {
@@ -31,24 +32,53 @@ func handleJavaClient(w http.ResponseWriter, r *http.Request) {
         "emergencyLevel": lvl,
     }
 
-    go broadcastInit(msgEmergency , address, id)
+    go broadcastInit(msgEmergency ,address, id)
 }
 
 func handleAndroidClient(w http.ResponseWriter, r *http.Request) {
     // recup l'id de l'emergency dans le message pour chopé le chan
-    vehiculeId := r.FormValue("vehiculeId")
+    idEmergency := r.FormValue("idEmergency")
     token := r.FormValue("token")
-    fmt.Printf(vehiculeId)
+    response := r.FormValue("response")
+    fmt.Printf(response)
+    fmt.Printf(idEmergency)
     fmt.Printf(token)
 
-    tokens = append(tokens, token)
+    rep := []{token, response}
+    repartiteur[idEmergency] <- rep
 }
 
-func broadcastInit(msg map[string]string, address string, id string) {
+func broadcastInit(msg map[string]string, address string, id string) { // id est aussi dans map...
     // init une liste de token potentiel pour l'intervention
     tokensPerimeter := spot(address, 2)
     memo[id] = tokensPerimeter
+    repartiteur[id] = make(chan string)
+    go listenResponse(id)
+    go sendAndroids(tokens, msg)
 
+}
+
+func listenResponse(id string, numberNecessary int) {
+    c := repartiteur[id]
+    inCharge := []string
+    for {
+        rep := <- c
+        switch rep[1] {
+        case "ok" :
+            inCharge = append(inCharge, rep[0])
+            t := []{rep[0]}
+            r := "go go go"
+            sendAndroids(t, r)
+        case "not" :
+            continue
+    }
+    if len(inCharge) == numberNecessary {
+        memo[id] = inCharge
+        return
+    }
+}
+
+func sendAndroids(tokens []string, msg string) {
     c := fcm.NewFcmClient(serverKey)
     c.NewFcmRegIdsMsg(tokens, msg)
     status, err := c.Send()
@@ -72,7 +102,7 @@ func catchGPS() {
         log.Fatal(err)
     }
     // a print avec [token, ...] histoire de dire qu'on recup toute la liste
-    fmt.Printf("%s\n", v)
+    fmt.Printf("%s\n", v) // oh le format degueu
 }
 
 func main() {
@@ -80,6 +110,8 @@ func main() {
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/android", handleAndroidClient).Methods("POST")
     router.HandleFunc("/java", handleJavaClient).Methods("POST")
+
+    catchGPS()
 
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
